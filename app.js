65af34205a4e990b5bfb4c945a22f2a0d7882d02
R1fky <rifkiyuda11@gmail.com>
2023-11-25 22:33:03 +0700

    memperbaiki pertanyaan

--- app.js
+++ app.js
@@ -19,16 +19,30 @@ if(!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-rl.question("siapa nama anda ?? ", (nama) => {
-  rl.question("masukkan no hp: ", (noHP) => {
-    const contact = { nama, noHP };
-    const file = fs.readFileSync("data/contacts.json", "utf-8");
-    const contacts = JSON.parse(file);
-
-    contacts.push(contact); //
-    fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
-
-    console.log("Terima kasih sudah memasukkan data");
-    rl.close();
+const tulisPertanyaan = (pertanyaan) => {
+  return new Promise((resolve, reject) => {
+    rl.question(pertanyaan, (nama) => {
+      resolve(nama);
+    });
   });
-});
+};
+
+const main = async () => {
+  const nama = await tulisPertanyaan('Masukkan nama Anda : ');
+  const email = await tulisPertanyaan('Masukkan email Anda : ');
+  const noHP = await tulisPertanyaan('Masukkan noHP Anda : ');
+
+  const contact = {nama, email, noHP};
+  const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
+  const contacts = JSON.parse(fileBuffer);
+
+  contacts.push(contact)
+  fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+
+  console.log("Terima kasih sudah memasukkan data");
+  rl.close();
+
+};
+
+main();
+
