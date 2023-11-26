3638a6dc0423fbf51a96b9e30d37877c7d2f55cb
R1fky <rifkiyuda11@gmail.com>
2023-11-26 14:31:21 +0700

    melakukan perubahan contactasynawait dan membedakan file proses contact

--- app.js
+++ app.js
@@ -1,47 +1,11 @@
-const fs = require("fs");
-
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-
-//membuat dir/folder data jika belum ada 
-const dirPath = './data';
-if(!fs.existsSync(dirPath)) {
-  fs.mkdirSync(dirPath);
-}
-
-//membuat file json jika belum ada 
-const dataPath = './data/contacts.json';
-if(!fs.existsSync(dataPath)) {
-  fs.writeFileSync(dataPath, '[]', 'utf-8');
-}
-
-const tulisPertanyaan = (pertanyaan) => {
-  return new Promise((resolve, reject) => {
-    rl.question(pertanyaan, (nama) => {
-      resolve(nama);
-    });
-  });
-};
+const contacts = require('./contacts');
 
 const main = async () => {
-  const nama = await tulisPertanyaan('Masukkan nama Anda : ');
-  const email = await tulisPertanyaan('Masukkan email Anda : ');
-  const noHP = await tulisPertanyaan('Masukkan noHP Anda : ');
+  const nama = await contacts.tulisPertanyaan('Masukkan nama Anda : ');
+  const email = await contacts.tulisPertanyaan('Masukkan email Anda : ');
+  const noHP = await contacts.tulisPertanyaan('Masukkan noHP Anda : ');
 
-  const contact = {nama, email, noHP};
-  const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
-  const contacts = JSON.parse(fileBuffer);
-
-  contacts.push(contact)
-  fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
-
-  console.log("Terima kasih sudah memasukkan data");
-  rl.close();
-
+  contacts.simpanContact(nama, email, noHP);
 };
 
 main();
