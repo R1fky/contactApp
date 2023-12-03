60e314972b89f1474d97cc4c5278cd6d596ed4f4
R1fky <rifkiyuda11@gmail.com>
2023-12-03 15:09:56 +0700

    menambahkan npm yargs

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const contacts = require('./contacts');
+const yargs = require("yargs");
 
-const main = async () => {
-  const nama = await contacts.tulisPertanyaan('Masukkan nama Anda : ');
-  const email = await contacts.tulisPertanyaan('Masukkan email Anda : ');
-  const noHP = await contacts.tulisPertanyaan('Masukkan noHP Anda : ');
+// mengambiil argument dari command line
+console.log(yargs.argv);
 
-  contacts.simpanContact(nama, email, noHP);
-};
 
-main();
+
+// const contacts = require('./contacts');
+// const main = async () => {
+//   const nama = await contacts.tulisPertanyaan('Masukkan nama Anda : ');
+//   const email = await contacts.tulisPertanyaan('Masukkan email Anda : ');
+//   const noHP = await contacts.tulisPertanyaan('Masukkan noHP Anda : ');
+
+//   contacts.simpanContact(nama, email, noHP);
+// };
+
+// main();
 
