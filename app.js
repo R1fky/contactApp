ab9e6b4a476692851abe53b888c4eda07b1a2ed0
R1fky <rifkiyuda11@gmail.com>
2023-12-07 23:29:52 +0700

    contactApp bagian2 fitur command diterminal untuk menambahkan contact gunakan yargs

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
-const yargs = require("yargs");
+const yargs = require('yargs');
+const contacts = require('./contacts');
 
 // mengambiil argument dari command line
-console.log(yargs.argv);
+// console.log(yargs.argv);
 
+yargs.command({
+    command: 'add',
+    description: 'menambahkan contact baru',
+    //builder ketika command dijalankan bisa menerima apa saja
+    builder: {
+        nama: {  //isinya adalah object
+            describe: "Nama Lengkap", //menjelaskan ini apa
+            demandOption: true, //harus diisi ketika bernilai true
+            type: 'string',
+        },
+        email: {
+            describe: "Email",
+            demandOption: false, //tidak wajib diisi karena bernilai false
+            type: 'string',
+        },
+        noHP: {
+            describe: "Nomor HandPhone",
+            demandOption: true,
+            type: 'string',
+        },
 
+    },
+    handler(argv) { //akan menerima argument
+        contacts.simpanContact(
+            argv.nama,
+            argv.email,
+            argv.noHP
+        );
+    },
+});
 
-// const contacts = require('./contacts');
-// const main = async () => {
-//   const nama = await contacts.tulisPertanyaan('Masukkan nama Anda : ');
-//   const email = await contacts.tulisPertanyaan('Masukkan email Anda : ');
-//   const noHP = await contacts.tulisPertanyaan('Masukkan noHP Anda : ');
-
-//   contacts.simpanContact(nama, email, noHP);
-// };
-
-// main();
+yargs.parse()
 
